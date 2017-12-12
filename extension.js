83f6b1812433e863e3b4b1e1cf210e3bbf42f588
Shehabul Hossain (SunNy) <rudrozzal@gmail.com>
2017-12-13 02:45:23 +0600

    version 0.9

--- extension.js
+++ extension.js
@@ -1,50 +1,116 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+var vscode = require('vscode');
+var fs = require('fs');
+var path = require('path');
+var events = require('events');
+var msg = require('./messages').messages;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+var fileUrl = require('file-url');
+
+const indicatorClass = '__CUSTOM_CSS_JS_INDICATOR_CLS'
+const indicatorCSS = `<link rel="stylesheet" type="text/css" href="${fileUrl(__dirname + '/glitch.css')}"/>`
+
 function activate(context) {
 
+	console.log('vscode-customcss is active!');
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "glitch" is now active!');
+	process.on('uncaughtException', function (err) {
+		if (/ENOENT|EACCES|EPERM/.test(err.code)) {
+			vscode.window.showInformationMessage(msg.admin);
+			return;
+		}
+	});
+
+	var eventEmitter = new events.EventEmitter();
+	var isWin = /^win/.test(process.platform);
+	var appDir = path.dirname(require.main.filename);
+
+	var base = appDir + (isWin ? '\\vs\\workbench' : '/vs/workbench');
+
+	var htmlFile = base + (isWin ? '\\electron-browser\\bootstrap\\index.html' : '/electron-browser/bootstrap/index.html');
+	var htmlFileBack = base + (isWin ? '\\electron-browser\\bootstrap\\index.html.bak-customcss' : '/electron-browser/bootstrap/index.bak-customcss');
+	
+	var preloadFile = base + (isWin ? '\\electron-browser\\bootstrap\\preload.js' : '/electron-browser/bootstrap/preload.js');
+	var preloadFileBack = base + (isWin ? '\\electron-browser\\bootstrap\\preload.js.bak-customcss' : '/electron-browser/bootstrap/preload.bak-customcss');
+
+	function editFiles() {
+		
+		try {
+			// update html to load css 
+			var html = fs.readFileSync(htmlFile, 'utf-8');
+			html = html.replace(/<!-- !! VSCODE-CUSTOM-CSS-START !! -->[\s\S]*?<!-- !! VSCODE-CUSTOM-CSS-END !! -->/, '');
+			html = html.replace(/(<\/html>)/,
+				'<!-- !! VSCODE-CUSTOM-CSS-START !! -->' + indicatorCSS + '<!-- !! VSCODE-CUSTOM-CSS-END !! --></html>');
+			fs.writeFileSync(htmlFile, html, 'utf-8');
+
+			// update preload file
+			var preload = fs.readFileSync(preloadFile, 'utf-8');
+			preload = preload.replace(/c.body.className="glitch /, 'c.body.className="');
+			preload = preload.replace(/c.body.className="/, 'c.body.className="glitch ');
+			fs.writeFileSync(preloadFile, preload, 'utf-8');
+			enabledRestart();
+
+		} catch (e) {
+			vscode.window.showInformationMessage(msg.admin);
+			return;
+		}
+    }
     
+	function restore() {
+		try {
+			// restore html to load css 
+			var html = fs.readFileSync(htmlFile, 'utf-8');
+			html = html.replace(/<!-- !! VSCODE-CUSTOM-CSS-START !! -->[\s\S]*?<!-- !! VSCODE-CUSTOM-CSS-END !! -->/, '');
+			fs.writeFileSync(htmlFile, html, 'utf-8');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+			// restore preload file
+			var preload = fs.readFileSync(preloadFile, 'utf-8');
+			preload = preload.replace(/c.body.className="glitch glitch/, 'c.body.className="');
+			fs.writeFileSync(preloadFile, preload, 'utf-8');
+			enabledRestart();
 
-        var decoratorType = vscode.window.createTextEditorDecorationType({
-            borderWidth: '1px',
-            borderStyle: 'solid',
-            overviewRulerColor: 'blue',
-            overviewRulerLane: vscode.OverviewRulerLane.Right,
-            light: {
-                // this color will be used in light color themes
-                borderColor: 'darkblue'
-            },
-            dark: {
-                // this color will be used in dark color themes
-                borderColor: 'lightblue'
-            }
-        });
+		} catch (e) {
+			vscode.window.showInformationMessage(msg.admin);
+			return;
+		}
+	}
 
-        let activeEditor = vscode.window.activeTextEditor;
-        activeEditor.setDecorations(decoratorType);
+	function reloadWindow() {
+		// reload vscode-window
+		vscode.commands.executeCommand("workbench.action.reloadWindow");
+	}
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+	function enabledRestart() {
+		vscode.window.showInformationMessage(msg.enabled, { title: msg.restartIde })
+			.then(function (msg) {
+				reloadWindow();
+			});
+	}
 
-    context.subscriptions.push(disposable);
+	function disabledRestart() {
+		vscode.window.showInformationMessage(msg.disabled, { title: msg.restartIde })
+			.then(function (msg) {
+				reloadWindow();
+			});
+	}
+
+	function enableGlitch(){
+		editFiles();
+	}
+
+	function disableGlitch(){
+		restore();
+	}
+
+	var enableGlitch = vscode.commands.registerCommand('extension.enableGlitch', enableGlitch);
+	var disableGlitch = vscode.commands.registerCommand('extension.disableGlitch', disableGlitch);
+
+	context.subscriptions.push(enableGlitch);
+	context.subscriptions.push(disableGlitch);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
+function deactivate() { 
+	restore();
 }
 exports.deactivate = deactivate;
---
+++ messages.js
@@ -0,0 +1,10 @@
+exports.messages = {
+	admin: 'Run VS Code with admin privileges so the changes can be applied.',
+	enabled: 'Glitch enabled. Restart to take effect. If Code complains about it is corrupted, CLICK DON\'T SHOW AGAIN. See README for more detail.',
+	disabled: 'Glitch disabled and reverted to default. Restart to take effect.',
+	already_disabled: 'Glitch already disabled.',
+	smthingwrong: 'Something went wrong: ',
+	restartIde: 'Restart Visual Studio Code',
+	notfound: 'Glitch not found.',
+	notconfigured: 'Glitch path not configured.'
+};
